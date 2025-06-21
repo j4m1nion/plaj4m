ce4bbb7c237ec5dfbc9ddf4a33e616b19d0ac82a
j4m1nion <mamila.xr@outlook.com>
2025-06-21 03:18:32 +0200

    added consumer-rules.pro

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ android {
 
     defaultConfig {
         minSdk = 26
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildFeatures {
