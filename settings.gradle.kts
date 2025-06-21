5b8e9c51b1f4fd3b4b2eb57f612d088c76520e66
j4m1nion <mamila.xr@outlook.com>
2025-06-21 02:10:36 +0200

    added missing files for build

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
