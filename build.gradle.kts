9fc38b5da4068d06261ea317f652265f36cc1de6
j4m1nion <mamila.xr@outlook.com>
2025-06-22 08:11:35 +0200

    updated version to 1.0.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
                 from(components["release"])
                 groupId = "com.github.j4m1nion"
                 artifactId = "j4player"
-                version = "1.0.13"
+                version = "1.0.14"
             }
         }
     }
